fc78f0d3c334228b7fcebc413621d9018d291311
Anjali <anjali@Anjalis-MacBook-Air.local>
2024-04-07 18:44:45 +0530

    imp

--- backend/model/userSchema.js
+++ backend/model/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrpt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+// here we are hashing the password
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrpt.hash(this.password, 12);
+    this.cpassword = await bcrpt.hash(this.cpassword, 12);
+  }
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
